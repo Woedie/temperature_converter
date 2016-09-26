751257f4a5153c1af1ad6a57daf3c360c7b2abb5
Milan Lamote <Milan Lamote>
2016-09-26 11:29:22 +0200

    cmd and txt ok, url on the go

--- app.rb
+++ app.rb
@@ -1,14 +1,12 @@
 require './lib/temperature_convert.rb'
 
-input = ARGV.first.to_f
+temp_converter = TemperatureConverter.new
 
-temp_converter = TemperatureConverter.new input
-temp_converter_txt = TemperatureConverter.new File.read('./data/temp.txt').to_f
-puts "Temperature cmdLine:"
-puts temp_converter.convertC
-puts temp_converter.convertF
-puts temp_converter.convertK
-puts "Temperature txtFile:"
-puts temp_converter_txt.convertC
-puts temp_converter_txt.convertF
-puts temp_converter_txt.convertK
+temp_converter.cmdLine_temp ARGV
+puts temp_converter.to_text
+
+temp_converter.txtFile_temp './data/temp.txt'
+puts temp_converter.to_text
+
+temp_converter.html_temp http://labict.be/software-engineering/temperature/api/temperature/fake
+puts temp_converter.to_text
--- lib/temperature_convert.rb
+++ lib/temperature_convert.rb
@@ -1,26 +1,31 @@
 
 class TemperatureConverter
 
-  def initialize temp
+  def cmdLine_temp argument
+
+    @temp = argument.first.to_f
+
+  end
+
+  def txtFile_temp location
+
+    @temp = File.read(location).to_f
+
+  end
+
+  def html_temp url
+
     @temp = temp
-  end
-
-  def convertF
-
-    ((@temp * (9.0/5.0) ) + 32).to_s + " Fahrenheit"
 
   end
 
-  def convertK
+  def to_text
 
+    "Temperature text:\r\n" +
+    @temp.to_s + " Celcius \r\n" +
+    ((@temp * (9.0/5.0) ) + 32).to_s + " Fahrenheit \r\n" +
     (@temp + 273.15).to_s + " Kelvin"
 
-  end
-
-  def convertC
-
-    @temp.to_s + " Celcius"
-    
   end
 
 end
