051b1c6352082ed93950a1c619cf93db72632bc7
Milan Lamote <milanlamote@hotmail.com>
2016-11-08 16:52:28 +0100

    Now reads all channels from TTN and shows json output

--- app.rb
+++ app.rb
@@ -1,21 +1,25 @@
-<<<<<<< HEAD
 require './lib/temperature_reader.rb'
 
 cmd = ARGV
 file = "./data/temp.txt"
 url = "http://labict.be/software-engineering/temperature/api/temperature/fake"
+ttn_link = "staging.thethingsnetwork.org"
+port = 18830
 
 reader = TemperatureReader.new
 
-puts "Commandline\n"
-reader.cmdline_temperature cmd
-puts '...............................................................'
+# puts "Commandline\n"
+# reader.cmdline_temperature cmd
+# puts '...............................................................'
+#
+#
+# puts "File"
+# reader.file_temperature file
+# puts '...............................................................'
+#
+# puts "Url\n"
+# reader.url_temperature url
+# puts '...............................................................'
 
-
-puts "File"
-reader.file_temperature file
-puts '...............................................................'
-
-puts "Url\n"
-reader.url_temperature url
-puts '...............................................................'
+puts "Listening on TTN\n"
+reader.ttn_reader
--- lib/temperature_reader.rb
+++ lib/temperature_reader.rb
@@ -1,6 +1,8 @@
 require 'net/http'
 require 'uri'
 require './lib/temperature_printer.rb'
+require 'rubygems'
+require 'mqtt'
 
 class TemperatureReader
 
@@ -28,6 +30,20 @@ class TemperatureReader
 
 		def open_url(url)
 			Net::HTTP.get(URI.parse(url))
+		end
+
+		def ttn_reader
+			# Subscribe example
+			MQTT::Client.connect(
+			:host => 'staging.thethingsnetwork.org',
+			:port => 1883,
+			:username => '70B3D57ED00012B2',
+			:password => 'c8iuTSccnypK1eoFzEb/OoqB2FVAiFg/aEaYesnNf4w=') do |c|
+			  # If you pass a block to the get method, then it will loop
+			  c.get('#') do |topic,message|
+			    puts "#{topic}: #{message}"
+			  end
+			end
 		end
 
 end
